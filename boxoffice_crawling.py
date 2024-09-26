5c4f6e98be45fd9c5ac5995c6329298d7c94f17b
yeonjae <pyj3740@gmail.com>
2024-09-26 16:47:25 +0900

    feat : boxoffice ranking crawling
    박스오피스 랭킹 크롤링 및 데이터베이스에 저장
    오래된 데이터는 삭제

--- boxoffice_crawling.py
+++ boxoffice_crawling.py
@@ -44,7 +44,7 @@ def base_boxoffice():
     일주일치 데이터베이스를 만들기 위한 반복문 작업
     이후에는 하루 단위로 진행 예정.
     """
-    for i in range(7, 0, -1):
+    for i in range(8, 1, -1):
         # api key
         key = env("KOFIC_API_KEY")
         # 어제 날짜
@@ -64,29 +64,33 @@ def base_boxoffice():
 
         # 해당 일의 1~10위 박스오피스 순위
         boxoffice_list = data.get("boxOfficeResult").get("dailyBoxOfficeList")  # [0]
-        # pprint.pprint(boxoffice_list)
+        # print(datetime.today())
+        # print("==========================")
+        # # pprint.pprint(boxoffice_list)
 
-        # print(datetime.today().year)
         for j in range(10):
             now_movie = boxoffice_list[j]
             title = now_movie.get("movieNm")
             rank = now_movie.get("rank")
 
             Ranking.objects.create(title=title, rank=rank, crawling_date=crawling_date)
+        # print("============= fin =============")
 
 
+# base_boxoffice()
 # 오래된 순위 10개의 데이터
 # old_data = Ranking.objects.filter(crawling_date="2024-09-25")
 # print(old_data)
 # old_data.delete()
 
 
-def boxoffice_rank():
-    # api key
-    key = env("KOFIC_API_KEY")
-    # 어제 날짜
-    yesterday = datetime.today() - timedelta(days=1)
+# api key
+key = env("KOFIC_API_KEY")
+# 어제 날짜
+yesterday = datetime.today() - timedelta(days=1)
 
+
+def boxoffice_rank():
     # 일주일치 데이터만 보관예정 : 일주일이 지난 데이터는 삭제
     delete_date = (yesterday - timedelta(days=8)).strftime("%Y-%m-%d")
     old_data = Ranking.objects.filter(crawling_date=delete_date)
@@ -105,7 +109,7 @@ def boxoffice_rank():
     data = response.json()
 
     # 해당 일의 1~10위 박스오피스 순위
-    boxoffice_list = data.get("boxOfficeResult").get("dailyBoxOfficeList")  # [0]
+    boxoffice_list = data.get("boxOfficeResult").get("dailyBoxOfficeList")
 
     for j in range(10):
         now_movie = boxoffice_list[j]
@@ -114,6 +118,8 @@ def boxoffice_rank():
 
         Ranking.objects.create(title=title, rank=rank, crawling_date=crawling_date)
 
+
+boxoffice_rank()
 
 # # step3.실행 주기 설정
 # schedule.every().day.at("12:00:00").do(boxoffice_rank)  # 매일 오후 12시에 실행
