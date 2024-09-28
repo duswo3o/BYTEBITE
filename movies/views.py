20f0c2cc208d3e81474e345a6fe79590cf66b643
jeil <sjl01002@gmail.com>
2024-09-28 16:14:39 +0900

    refactor:db 생성 로직을 명령으로 수정
    기존에 api/v1/movies/database/로 POST 요청을 보내어 처리하던 db 업데이트
    로직을 터미널에서 python manage.py updatedb 라는 명령으로 실행할 수
    있도록 수정했습니다.

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,6 @@
 # 표준 라이브러리
-import time
 
 # 서드파티 라이브러리
-import requests
 from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -10,11 +8,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Django 기능 및 프로젝트 관련
-from django.conf import settings
-from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
-from .models import Genre, Movie, Ranking, Rating, Staff
+from .models import Movie, Ranking, Rating
 from .serializers import BoxofficeSerializer, MovieSerializer
 
 
@@ -100,130 +96,3 @@ class MovieScoreAPIView(APIView):
                 {"detail": f"이 영화의 점수를 {score}로 평가합니다."},
                 status=status.HTTP_200_OK
                 )
-
-
-class MovieDataBaseAPIView(APIView):
-    """데이터베이스 최신화 요청 클래스
-
-    현재 버전에서는 테스트용 데이터가 필요하기 때문에 개발자가 원하는 시점에
-    요청을 보내서 DB를 업데이트 할 수 있도록 함.
-    추후에는 삭제 또는 수정할 예정입니다.
-    """
-    API_URL = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2"
-
-    def post(self, request):
-        start_count = 0
-        total_data = []
-
-        while True:
-            params = {
-                "ServiceKey": settings.KMDB_API_KEY,
-                "listCount": 100,
-                "startCount": start_count,
-                "detail": "N",
-            }
-
-            response = requests.get(self.API_URL, params=params)
-
-            if response.status_code == 200:
-                data = response.json()["Data"][0]["Result"]
-                total_data.extend(data)
-
-                if len(data) < 1000:
-                    break
-                # 아래의 break문을 주석처리 하면 KMDb의 모든 데이터를 가져오게 됩니다.
-                break
-
-                start_count += 1000
-
-                time.sleep(3)
-
-            else:
-                return Response(
-                    {"error": "API에 접속에 실패했습니다."},
-                    status=status.HTTP_400_BAD_REQUEST
-                    )
-
-        save_to_database(total_data)
-        return Response(
-            {"message": f"{len(total_data)}개의 영화 데이터가 저장되었습니다."},
-            status=status.HTTP_200_OK
-            )
-
-
-def save_to_database(total_data):
-    for item in total_data:
-        movie_cd = item["movieSeq"]
-        title = item["title"]
-        runtime = item["runtime"] if item["runtime"] else None
-        rating = item["rating"] if item["rating"] else None
-        plot = item["plots"]["plot"][0]["plotText"]
-
-        genres = item["genre"].split(",")
-        genre_objects = []
-
-        for genre_name in genres:
-            genre_name = genre_name.strip()
-            genre, created = Genre.objects.get_or_create(name=genre_name)
-            genre_objects.append(genre)
-
-        movie, created = Movie.objects.update_or_create(
-            movie_cd=movie_cd,
-            defaults={
-                "title": title,
-                "runtime": runtime,
-                "grade": rating,
-                "plot": plot,
-                }
-            )
-
-        movie.genre.set(genre_objects)
-
-        directors = item["directors"]["director"]
-        actors = item["actors"]["actor"]
-
-        create_staff(movie, directors, actors)
-
-        movie.save()
-
-
-def create_staff(movie, directors, actors):
-    staffs = []
-
-    for director in directors:
-        staffs.append(
-            {"name_cd": director["directorId"],
-                "name": director["directorNm"],
-                "role": "director"}
-            )
-
-    for actor in actors:
-        staffs.append(
-            {"name_cd": actor["actorId"],
-                "name": actor["actorNm"],
-                "role": "actor"}
-            )
-
-    for staff in staffs:
-        name_cd = staff.get("name_cd")
-        name_cd = int(name_cd) if name_cd else None
-
-        name = staff.get("name")
-        role = staff.get("role")
-
-        try:
-            if name_cd is not None:
-                staff, created = Staff.objects.get_or_create(
-                    name_cd=name_cd,
-                    defaults={"name": name, "role": role}
-                    )
-            else:
-                staff, created = Staff.objects.get_or_create(
-                    name=name,
-                    role=role
-                    )
-
-            movie.staffs.add(staff)
-
-        except IntegrityError:
-            pass
