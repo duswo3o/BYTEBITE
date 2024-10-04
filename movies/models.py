d89e80910e025351a3932222f9c5e7dcce4c935d
jeil <sjl01002@gmail.com>
2024-10-04 16:15:44 +0900

    fix: Ranking 테이블에 movie_pk필드를 추가
    Movie 테이블과 연결하기 위함입니다.

--- movies/models.py
+++ movies/models.py
@@ -8,6 +8,7 @@ class Ranking(models.Model):
     title = models.CharField(max_length=255)
     rank = models.PositiveIntegerField()
     crawling_date = models.DateField()
+    movie_pk = models.IntegerField(unique=True, blank=True)
 
     def __str__(self):
         return self.title
