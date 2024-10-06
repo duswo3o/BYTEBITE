44d28c3c949a1c1e61f295df561c9b97e5e480d2
l-yoon <min>
2024-10-07 03:16:55 +0900

    add: ip address added
    마지막으로 저희 도메인을(popcorngeek.store) 허용해보겠습니다... 정말 되면 좋겠습니다.
    이걸 이렇게 main에다가 막 변경하고 그래도 되는지는 의문이지만 이제 이게 저의 배포에 관한 마지막 commit이라 생각하며 올립니다

--- front/movies/movies.js
+++ front/movies/movies.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://43.201.150.34/api/v1/'
+const API_BASE_URL = 'https://popcorngeek.store/api/v1/'
 
 // 토큰 저장 및 관리 함수
 const tokenManager = {
@@ -85,7 +85,7 @@ axios.get(`${API_BASE_URL}movies/`)
             const li = document.createElement('li');
 
             const gradelink = document.createElement('a');
-            gradelink.href = `https://43.201.150.34/front/movies/details.html?pk=${movie.id}`;
+            gradelink.href = `https://popcorngeek.store/front/movies/details.html?pk=${movie.id}`;
             gradelink.textContent = movie.title;
 
             li.appendChild(gradelink);
@@ -101,7 +101,7 @@ axios.get(`${API_BASE_URL}movies/`)
             const li = document.createElement('li');
 
             const likelink = document.createElement('a');
-            likelink.href = `https://43.201.150.34/front/movies/details.html?pk=${movie.id}`;
+            likelink.href = `https://popcorngeek.store/front/movies/details.html?pk=${movie.id}`;
             likelink.textContent = movie.title;
 
             li.appendChild(likelink);
@@ -156,7 +156,7 @@ if (searchKeyword) {
                     const genreNames = item.genre.map(genre => genre.name).join(', ');
 
                     const searchlink = document.createElement('a');
-                    searchlink.href = `https://43.201.150.34/front/movies/details.html?pk=${item.id}`;
+                    searchlink.href = `https://popcorngeek.store/front/movies/details.html?pk=${item.id}`;
                     searchlink.textContent = item.title;
 
                     li.appendChild(searchlink);
